79ee83b4580b501268fad2a65f0eed9b52014e40
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2023-12-06 23:27:14 -0600

    newBeginning project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Form, Badge, Button} from 'react-bootstrap';
+import axios from 'axios';
+
+const postData = async (url, data) => {
+  try {
+    const response = await axios.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error(`Error in postData: ${error}`);
+  }
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const name = event.target.elements.name.value;
+  const email = event.target.elements.email.value;
+  const message = event.target.elements.message.value;
+  const data = { name, email, message };
+  const response = await postData('https://fm56pa3fp6.execute-api.us-east-1.amazonaws.com/add-contact', data);
+  console.log(response);
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1>
+        Contact Form
+      </h1>
+      <br>
+      </br>
+      <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Enter your name</Form.Label>
+          <Form.Control name="name" type="name" placeholder="Enter your name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control name="email" type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Message</Form.Label>
+          <Form.Control name="message" type="message" as="textarea" rows={3} placeholder="Your message"/>
+        </Form.Group>
+        <Button type="submit" variant="primary">Submit</Button>
+      </Form>
     </div>
   );
 }
